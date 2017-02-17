a371d0dab461a059d91a35bdf305c3537c80672d
jsrhu <joshua.s.r.hu@gmail.com>
2017-02-17 12:33:01 -0500

    changed unaccepted character in method docstring of getAccountInformation()

--- api/broker.py
+++ api/broker.py
@@ -283,58 +283,58 @@ class IBDataBroker(IBBroker, DataBroker):
     def getAccountInformation(self, all_accounts = True, attributes = ','):
         """
         attributes values:
-        AccountType — Identifies the IB account structure
-        NetLiquidation — The basis for determining the price of the assets in
+        AccountType - Identifies the IB account structure
+        NetLiquidation - The basis for determining the price of the assets in
             your account. Total cash value + stock value + options value + bond value
-        TotalCashValue — Total cash balance recognized at the time of trade
+        TotalCashValue - Total cash balance recognized at the time of trade
             + futures PNL
-        SettledCash — Cash recognized at the time of settlement
+        SettledCash - Cash recognized at the time of settlement
             - purchases at the time of trade - commissions - taxes - fees
-        AccruedCash — Total accrued cash value of stock, commodities
+        AccruedCash - Total accrued cash value of stock, commodities
             and securities
-        BuyingPower — Buying power serves as a measurement of the dollar value
+        BuyingPower - Buying power serves as a measurement of the dollar value
             of securities that one may purchase in a securities account
             without depositing additional funds
-        EquityWithLoanValue — Forms the basis for determining whether a client
+        EquityWithLoanValue - Forms the basis for determining whether a client
             has the necessary assets to either initiate
             or maintain security positions. Cash + stocks + bonds + mutual funds
-        PreviousEquityWithLoanValue — Marginable Equity with Loan value
+        PreviousEquityWithLoanValue - Marginable Equity with Loan value
             as of 16:00 ET the previous day
-        GrossPositionValue — The sum of the absolute value
+        GrossPositionValue - The sum of the absolute value
             of all stock and equity option positions
-        RegTEquity — Regulation T equity for universal account
-        RegTMargin — Regulation T margin for universal account
-        SMA — Special Memorandum Account: Line of credit created
+        RegTEquity - Regulation T equity for universal account
+        RegTMargin - Regulation T margin for universal account
+        SMA - Special Memorandum Account: Line of credit created
             when the market value of securities in a Regulation T account
             increase in value
-        InitMarginReq — Initial Margin requirement of whole portfolio
-        MaintMarginReq — Maintenance Margin requirement of whole portfolio
-        AvailableFunds — This value tells what you have available for trading
-        ExcessLiquidity — This value shows your margin cushion,
+        InitMarginReq - Initial Margin requirement of whole portfolio
+        MaintMarginReq - Maintenance Margin requirement of whole portfolio
+        AvailableFunds - This value tells what you have available for trading
+        ExcessLiquidity - This value shows your margin cushion,
             before liquidation
-        Cushion — Excess liquidity as a percentage of net liquidation value
-        FullInitMarginReq — Initial Margin of whole portfolio
+        Cushion - Excess liquidity as a percentage of net liquidation value
+        FullInitMarginReq - Initial Margin of whole portfolio
             with no discounts or intraday credits
-        FullMaintMarginReq — Maintenance Margin of whole portfolio
+        FullMaintMarginReq - Maintenance Margin of whole portfolio
             with no discounts or intraday credits
-        FullAvailableFunds — Available funds of whole portfolio
+        FullAvailableFunds - Available funds of whole portfolio
             with no discounts or intraday credits
-        FullExcessLiquidity — Excess liquidity of whole portfolio
+        FullExcessLiquidity - Excess liquidity of whole portfolio
             with no discounts or intraday credits
-        LookAheadNextChange — Time when look-ahead values take effect
-        LookAheadInitMarginReq — Initial Margin requirement of whole portfolio
+        LookAheadNextChange - Time when look-ahead values take effect
+        LookAheadInitMarginReq - Initial Margin requirement of whole portfolio
             as of next period's margin change
-        LookAheadMaintMarginReq — Maintenance Margin requirement
+        LookAheadMaintMarginReq - Maintenance Margin requirement
             of whole portfolio as of next period's margin change
-        LookAheadAvailableFunds — This value reflects your available funds
+        LookAheadAvailableFunds - This value reflects your available funds
             at the next margin change
-        LookAheadExcessLiquidity — This value reflects your excess liquidity
+        LookAheadExcessLiquidity - This value reflects your excess liquidity
             at the next margin change
-        HighestSeverity — A measure of how close the account is to liquidation
-        DayTradesRemaining — The Number of Open/Close trades a user
+        HighestSeverity - A measure of how close the account is to liquidation
+        DayTradesRemaining - The Number of Open/Close trades a user
             could put on before Pattern Day Trading is detected.
             A value of "-1" means that the user can put on unlimited day trades.
-        Leverage — GrossPositionValue / NetLiquidation
+        Leverage - GrossPositionValue / NetLiquidation
         """
 
         self.tws.reqAccountSummary(reqId = self.current_request_id,
