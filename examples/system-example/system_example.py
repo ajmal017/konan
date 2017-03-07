89e4e31fdb1dcc482f719a907db54b7aae6d27f4
jsrhu <joshua.s.r.hu@gmail.com>
2017-03-07 16:20:04 -0500

    Added some changes to resource initialization.

--- examples/system-example/system_example.py
+++ examples/system-example/system_example.py
@@ -5,8 +5,10 @@ system-example.py
 Created on 2017-03-03T13:17:00Z
 @author:Joshua Hu
 """
+# imports from future
 from __future__ import print_function
 
+#imports from stdlib
 import abc
 
 import time
@@ -14,18 +16,26 @@ import datetime as dt
 
 import cPickle
 
+# internal/custom imports
 import konan.api.system as system
+import konan.api.broker as broker
 
 import strategy_example
 
 path_system_state = '/Users/jsrhu/Dropbox/'
 
 def main():
+    # initialize the broker resource
+    eBroker = broker.IBBrokerTotal()
+
+    # specify strategies associated with the system account
+    dict_strategies = {'eStrategy': strategy_example.exampleStrategy()}
+
+    # create
     eSystem = system.System(path_system_state = '',
-                            strategies = {'eStrategy': strategy_example.exampleStrategy()},
-                            time_sleep = 10)
-    # broker parameter passed default value of IBBrokerTotal() class
-    # from broker module
+                            strategies = dict_strategies,
+                            time_end = dt.time(hour = 16, minute = 30),
+                            time_sleep = 10, broker = eBroker)
 
     for event in eSystem.strategy_schedule:
         # TODO: modularize
