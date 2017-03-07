eaffe0ab457e9c479d1d13c8d754d4bca02286fa
jsrhu <joshua.s.r.hu@gmail.com>
2017-03-07 16:55:25 -0500

    Added <time_sleep> property; implemented main run method.

--- api/strategy.py
+++ api/strategy.py
@@ -11,6 +11,7 @@ from __future__ import print_function
 #imports from stdlib
 import abc
 
+import time
 import datetime as dt
 
 # internal/custom imports
@@ -21,14 +22,23 @@ class Strategy(object):
     """docstring for Strategy."""
     __metaclass__ = abc.ABCMeta
 
-    def __init__(self, broker = broker.IBBrokerTotal(), decision_algorithm = None, portfolio = None,
-                    time_execution = None):
+    def __init__(self, broker = None, decision_algorithm = None,
+                    portfolio = None, time_execution = None, time_end = None,
+                    time_sleep = None, event_schedule = None):
         super(Strategy, self).__init__()
 
         self._broker = broker
+
         self._decision_algorithm = decision_algorithm
+
         self._portfolio = portfolio
+
         self._time_execution = time_execution
+        self._time_end = time_end
+
+        self._time_sleep = 0#time_sleep
+
+        self._event_schedule = event_schedule
 
     """
     CLASS PROPERTIES
@@ -77,22 +87,59 @@ class Strategy(object):
         return locals()
     time_execution = property(**time_execution())
 
-    @abc.abstractmethod
+    def time_end():
+        doc = "The time_end property."
+        def fget(self):
+            return self._time_end
+        def fset(self, value):
+            self._time_end = value
+        def fdel(self):
+            del self._time_end
+        return locals()
+    time_end = property(**time_end())
+
+    def time_sleep():
+        doc = "The time_sleep property."
+        def fget(self):
+            return self._time_sleep
+        def fset(self, value):
+            self._time_sleep = value
+        def fdel(self):
+            del self._time_sleep
+        return locals()
+    time_sleep = property(**time_sleep())
+
+    def event_schedule():
+        doc = "The event_schedule property."
+        def fget(self):
+            return self._event_schedule
+        def fset(self, value):
+            self._event_schedule = value
+        def fdel(self):
+            del self._event_schedule
+        return locals()
+    event_schedule = property(**event_schedule())
+
     def checkPortfolio():
-        raise NotImplementedError("checkPortfolio() has not been implemented")
+        raise NotImplementedError("checkPortfolio() has not been implemented in the API")
 
-    @abc.abstractmethod
     def checkDecision():
-        raise NotImplementedError("checkDecision() has not been implemented")
+        raise NotImplementedError("checkDecision() has not been implemented in the API")
 
-    @abc.abstractmethod
     def makeTrade(position = position.Position()):
-        raise NotImplementedError("makeTrade() has not been implemented")
+        raise NotImplementedError("makeTrade() has not been implemented in the API")
 
-    @abc.abstractmethod
     def updatePortfolio(position = position.Position()):
-        raise NotImplementedError("updatePortfolio() has not been implemented")
+        raise NotImplementedError("updatePortfolio() has not been implemented in the API")
 
-    @abc.abstractmethod
-    def execute():
-        raise NotImplementedError("execute() has not been implemented")
+    def execute(self):
+        print('EXECUTING: ', self.__class__.__name__)
+
+        while dt.datetime.now().time() <= self.time_end:
+            for event in self.event_schedule:
+                # current <event_schedule> structure based on old <System> <run> method
+                if dt.datetime.now().time() >= dt.datetime.strptime(str(event), '%H:%M:%S.%f').time():
+                    print(event)
+                    print(self.event_schedule[event][0],':',self.event_schedule[event][1])
+                    self.event_schedule[event][0]()#(self.event_schedule[event][1])
+            time.sleep(self.time_sleep)
